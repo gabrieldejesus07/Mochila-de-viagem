b98470f4ad54d9b683b61e50b3b3a75e2c62defe
Gabriel de Jesus <contatogabrieldejesusaraujo@gmail.com>
2023-03-16 16:28:08 -0300

    Ajustando erros

--- js/main.js
+++ js/main.js
@@ -1,20 +1,27 @@
 const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
 
 form.addEventListener("submit", (evento) => {
    
     evento.preventDefault()
+    
     criarElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+    
+
 })
 
 function criarElemento(nome, quantidade) {
-    const novoItem = document.createElement('li')
-    novoItem.classList.add("item")
+   
+   const novoItem = document.createElement('li')
+   novoItem.classList.add("item")
 
-    const numeroItem = document.createElement('strong')
-    numeroItem.innearHTML = quantidade
+   const numeroItem = document.createElement('strong')
+   numeroItem.innerHTML = quantidade
 
-    novoItem.appendChild(numeroItem)
-    novoItem.innerHTML += nome
-    
-    console.log(novoItem)
+   novoItem.appendChild(numeroItem)
+   novoItem.innerHTML += nome
+
+   lista.appendChild(novoItem)
+
+   
 }
